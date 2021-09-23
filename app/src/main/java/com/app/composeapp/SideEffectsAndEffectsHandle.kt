640d54b9ea9cf6bf36d41ff930f499b1ddc760c3
Oksana Lugovaya <otl@fivegen.ru>
2021-09-23 22:51:01 +0300

    add

---
+++ app/src/main/java/com/app/composeapp/SideEffectsAndEffectsHandle.kt
@@ -0,0 +1,16 @@
+package com.app.composeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class SideEffectsAndEffectsHandle : ComponentActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
--- app/src/main/java/com/app/composeapp/StyleActivity.kt
+++ app/src/main/java/com/app/composeapp/StyleActivity.kt
@@ -1,7 +1,6 @@
 package com.app.composeapp
 
 import android.annotation.SuppressLint
-import android.graphics.Typeface
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,9 +8,11 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
