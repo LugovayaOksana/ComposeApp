c074ac489b4285793fbf0d9de8d59ac26d163fe1
Oksana Lugovaya <otl@fivegen.ru>
2021-09-24 09:52:38 +0300

    add

--- app/src/main/java/com/app/composeapp/SideEffectsAndEffectsHandle.kt
+++ app/src/main/java/com/app/composeapp/SideEffectsAndEffectsHandle.kt
@@ -2,15 +2,82 @@ package com.app.composeapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.compose.setContent
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SideEffectsAndEffectsHandle : ComponentActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val scaffoldState = rememberScaffoldState()
+            val scope = rememberCoroutineScope()
+            Scaffold(scaffoldState = scaffoldState) {
+                var counter = produceState(initialValue = 0) {
+                    delay(3000L)
+                    value = 4
+                }
+                if (counter.value % 5 == 0 && counter.value > 0) {
+                    LaunchedEffect(key1 = scaffoldState.snackbarHostState) {
+                        scaffoldState.snackbarHostState.showSnackbar("Hello")
+                    }
+                }
+                Button(onClick = { }) {
+                    Text("Click me: ${counter.value}")
+                }
+
+
+//                var counter by remember {
+//                    mutableStateOf(0)
+//                }
+//                if(counter % 5 == 0 && counter > 0){
+                //1
+//                    scope.launch {
+//                        scaffoldState.snackbarHostState.showSnackbar("Hello")
+//                    }
+                //2
+//                    LaunchedEffect(key1 = scaffoldState.snackbarHostState) {
+//                        scaffoldState.snackbarHostState.showSnackbar("Hello")
+//                    }
+//                }
+//                Button(onClick = { counter++ }) {
+//                    Text("Click me: $counter")
+//                }
+            }
+
 
         }
+    }
+}
+
+var i = 0
+
+@Composable
+fun MyComposable(backPressedDispatcher: OnBackPressedDispatcher) {
+
+    val callback = remember {
+        object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                TODO("Not yet implemented")
+            }
+        }
+    }
+
+    DisposableEffect(key1 = backPressedDispatcher) {
+        backPressedDispatcher.addCallback(callback)
+        onDispose {
+            callback.remove()
+        }
+    }
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = "Click me")
     }
 }
